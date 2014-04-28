5f86262f0ad548209e6bd83915645027dc9967c6
Tamim Sookoor <tamimsookoor@gmail.com>
2014-04-27 23:42:16 -0400

    trying out authorizer

--- publications.php
+++ publications.php
@@ -1,91 +1,97 @@
 <?php
-$thisPage="Publications";
+   $thisPage="Publications";
 include("phpincludes/header.php");
-?>
-										<section>
-											<h3>2013</h3>
+   ?>
+<section>
+  <h3>2013</h3>
+  
+  
+  <ul>
 
 
-<ul>
-
-<li>Tamim Sookoor and Kamin Whitehouse. <a href="docs/sookoor13roomZoner.pdf" onClick="javascript:pageTracker._trackPageview('docs/sookoor13roomZoner.pdf'); ">RoomZoner: Occupancy-based Room-Level Zoning of a Centralized HVAC System</a>. The 4th International Conference on Cyber-Physical Systems (ICCPS'13). April 8-11, 2013, Philadelphia, PA. (<a href="docs/sookoor13iccpsFinal.pptx" onClick="javascript:pageTracker._trackPageview('docs/sookoor13iccpsFinal.pptx'); ">ppt</a>) [23% acceptance rate]</li>
-
-<br />
-
-<li>Tamim Sookoor, Brian Holben, and Kamin Whitehouse. <a href="docs/sookoor-suscom.pdf" onClick="javascript:pageTracker._trackPageview('docs/sookoor-suscom.pdf'); ">Feasibility of Retrofitting Centralized HVAC Systems for Room-Level Zoning</a>. Elsevier Journal on Sustainable Computing, Informatics, and Systems. 2013.</li>    
-
-</ul>
-
-											<h3>2012</h3>
-<ul>
-<li>Virginia Smith, Tamim Sookoor, and Kamin Whitehouse. <a href="docs/smith12sigbed.pdf" onClick="javascript:pageTracker._trackPageview('docs/smith12sigbed.pdf'); ">Modeling Building Thermal Response to HVAC Zoning</a>. ACM SIGBED Review, Special Issue on Networks of Cooperating Objects. September, 2012.</li>
-
-   <br />  
-
-<li>Kamin Whitehouse, Juhi Ranjan, Jiakang Lu, Tamim Sookoor, Mehdi Sadaat, Carrie Burke, Galen Staengle, Anselmo Canfora, and Hossein Haj-Hariri. <a href="docs/whitehouse12occupancyDriven.pdf" onClick="javascript:pageTracker._trackPageview('docs/whitehouse12occupancyDriven.pdf'); ">Towards Occupancy-driven Heating and Cooling</a>. IEEE Design and Test of Computers, Special Issue on Green Electronics and Computing. July/August, 2012.</li>
-
-   <br />	    
-
-<li>Tamim Sookoor, Brian Holben, and Kamin Whitehouse. <a href="docs/sookoor12feasibility.pdf" onClick="javascript:pageTracker._trackPageview('docs/sookoor12feasibility.pdf'); ">Feasibility of Retrofitting Centralized HVAC Systems for Room-Level Zoning</a>. The Third International Green Computing Conference (IGCC'12). June 5-8, 2012, San Jose, CA. (<a href="docs/sookoor12feasibility.key" onClick="javascript:pageTracker._trackPageview('docs/sookoor12feasibility.key'); ">key</a>, <a href="docs/sookoor12feasibility.ppt" onClick="javascript:pageTracker._trackPageview('docs/sookoor12feasibility.ppt'); ">ppt</a>)</li>
-      
-   <br />
-
+    <!-- ACM DL Article: RoomZoner: occupancy-based room-level zoning of a centralized HVAC system-->
+    <div class="acmdlitem" id="item2502553"><img src="http://dl.acm.org/images/oa.gif" width="25" height="25" border="0" alt="ACM DL Author-ize service" style="vertical-align:middle"/><a href="http://dl.acm.org/authorize?6836387" title="RoomZoner: occupancy-based room-level zoning of a centralized HVAC system">RoomZoner: occupancy-based room-level zoning of a centralized HVAC system</a><div style="margin-left:25px"><a href="http://dl.acm.org/author_page.cfm?id=81384616704" >Tamim Sookoor</a>, <a href="http://dl.acm.org/author_page.cfm?id=81100551318" >Kamin Whitehouse</a><br />ICCPS '13 Proceedings of the ACM/IEEE 4th International Conference on Cyber-Physical Systems, 2013</div></div>
+    <!-- ACM DL Bibliometrics: RoomZoner: occupancy-based room-level zoning of a centralized HVAC system-->
+    <div class="acmdlstat" id ="stats2502553"><iframe src="http://dl.acm.org/authorizestats?6836387" width="100%" height="30" scrolling="no" frameborder="0">frames are not supported</iframe></div> 
+    
+    <!-- <li>Tamim Sookoor and Kamin Whitehouse. <a href="docs/sookoor13roomZoner.pdf" onClick="javascript:pageTracker._trackPageview('docs/sookoor13roomZoner.pdf'); ">RoomZoner: Occupancy-based Room-Level Zoning of a Centralized HVAC System</a>. The 4th International Conference on Cyber-Physical Systems (ICCPS'13). April 8-11, 2013, Philadelphia, PA. (<a href="docs/sookoor13iccpsFinal.pptx" onClick="javascript:pageTracker._trackPageview('docs/sookoor13iccpsFinal.pptx'); ">ppt</a>) [23% acceptance rate]</li> -->
+    
+    <br />
+    
+    <li>Tamim Sookoor, Brian Holben, and Kamin Whitehouse. <a href="docs/sookoor-suscom.pdf" onClick="javascript:pageTracker._trackPageview('docs/sookoor-suscom.pdf'); ">Feasibility of Retrofitting Centralized HVAC Systems for Room-Level Zoning</a>. Elsevier Journal on Sustainable Computing, Informatics, and Systems. 2013.</li>    
+    
+  </ul>
+  
+  <h3>2012</h3>
+  <ul>
+    <li>Virginia Smith, Tamim Sookoor, and Kamin Whitehouse. <a href="docs/smith12sigbed.pdf" onClick="javascript:pageTracker._trackPageview('docs/smith12sigbed.pdf'); ">Modeling Building Thermal Response to HVAC Zoning</a>. ACM SIGBED Review, Special Issue on Networks of Cooperating Objects. September, 2012.</li>
+    
+    <br />  
+    
+    <li>Kamin Whitehouse, Juhi Ranjan, Jiakang Lu, Tamim Sookoor, Mehdi Sadaat, Carrie Burke, Galen Staengle, Anselmo Canfora, and Hossein Haj-Hariri. <a href="docs/whitehouse12occupancyDriven.pdf" onClick="javascript:pageTracker._trackPageview('docs/whitehouse12occupancyDriven.pdf'); ">Towards Occupancy-driven Heating and Cooling</a>. IEEE Design and Test of Computers, Special Issue on Green Electronics and Computing. July/August, 2012.</li>
+    
+    <br />	    
+    
+    <li>Tamim Sookoor, Brian Holben, and Kamin Whitehouse. <a href="docs/sookoor12feasibility.pdf" onClick="javascript:pageTracker._trackPageview('docs/sookoor12feasibility.pdf'); ">Feasibility of Retrofitting Centralized HVAC Systems for Room-Level Zoning</a>. The Third International Green Computing Conference (IGCC'12). June 5-8, 2012, San Jose, CA. (<a href="docs/sookoor12feasibility.key" onClick="javascript:pageTracker._trackPageview('docs/sookoor12feasibility.key'); ">key</a>, <a href="docs/sookoor12feasibility.ppt" onClick="javascript:pageTracker._trackPageview('docs/sookoor12feasibility.ppt'); ">ppt</a>)</li>
+    
+    <br />
+    
 <li>Ginger Smith, Tamim Sookoor, and Kamin Whitehouse. <a href="docs/smith12conet.pdf" onClick="javascript:pageTracker._trackPageview('docs/smith12conet.pdf'); ">Modeling Building Thermal Response to HVAC Zoning</a>. The Third International Workshop on Networks of Cooperating Objects (CONET'12). Co-located with the CPS Week 2012. April 16th, 2012. Beijing, China. (<a href="docs/smith12conet.key" onClick="javascript:pageTracker._trackPageview('docs/smith12conet.key'); ">key</a>, <a href="docs/smith12conet.pptx" onClick="javascript:pageTracker._trackPageview('docs/smith12conet.pptx'); ">ppt</a>)</li>
 
-   <br />
-
-<li>Tamim Sookoor. <a href="docs/sookoor-phd-dissertation.pdf" onClick="javascript:pageTracker._trackPageview('docs/sookoor-phd-dissertation.pdf'); ">Application Development for Cyber-Physical Systems: Programming Language Concepts and Case Studies</a>. Ph.D. dissertation, University of Virginia, 2012.</li>
-</ul>
-
-
-      <h3>2011</h3>
-
-<ul>
-<li>Timothy Hnat, Vijay Srinivasan, Jiakang Lu, Tamim Sookoor, Raymond Dawson, John Stankovic, and Kamin Whitehouse. <a href="docs/hnat11hitchhikers.pdf" onClick="javascript:pageTracker._trackPageview('docs/hnat11hitchhikers.pdf'); ">The Hitchhiker's Guide to Successful Residental Sensing Deployments</a>. The 9th ACM Conference on Embedded Networked Sensing Systems (SenSys'11). November 2-4, 2011, Seattle, WA. (<a href="docs/hnat-sensys11.pptx" onClick="javascript:pageTracker._trackPageview('docs/hnat-sensys11.pptx'); ">ppt</a>) [19.5% acceptance rate]
-</li>
-</ul>
-
-<h3>2010</h3>
-
-<ul>
-<li>Jiakang Lu, Tamim Sookoor, Vijay Srinivasan, Gao Ge, Brian Holben, John Stankovic, Eric Field, and Kamin Whitehouse. <a href="docs/sensys10thermostat.pdf" onClick="javascript:pageTracker._trackPageview('docs/sensys10thermostat.pdf'); ">The Smart Thermostat: Using Occupancy Sensors to Save Energy in Homes</a>. The 8th ACM Conference on Embedded Networked Sensing Systems (SenSys'10). November 3-5, 2010, Zurich, Switzerland. (<a href="docs/sensys10lu_SmartThermostat-talk.ppt" onClick="javascript:pageTracker._trackPageview('docs/sensys10lu_SmartThermostat-talk.ppt'); ">ppt</a>) [17% acceptance rate]</li>
-
-
 <br />
 
-<li>Timothy W. Hnat, Tamim I. Sookoor, Pieter Hooimeijer, Westley Weimer, and Kamin Whitehouse. <a href="docs/hnat10compiler.pdf" onClick="javascript:pageTracker._trackPageview('docs/hnat10compiler.pdf'); ">A Modular and Extensible Macroprogramming Compiler</a>. The 1st Workshop on Software Engineering for Sensor Network Applications (SESENA) in Conjunction with ACM/IEEE International Conference on Software Engineering (ICSE), May 2010 Cape Town, South Africa. (<a href="docs/sookoor13iccpsFinal.pptx" onClick="javascript:pageTracker._trackPageview('docs/sookoor13iccpsFinal.pptx'); ">ppt</a>) </li>
-</ul>
+    <li>Tamim Sookoor. <a href="docs/sookoor-phd-dissertation.pdf" onClick="javascript:pageTracker._trackPageview('docs/sookoor-phd-dissertation.pdf'); ">Application Development for Cyber-Physical Systems: Programming Language Concepts and Case Studies</a>. Ph.D. dissertation, University of Virginia, 2012.</li>
+  </ul>
+  
+  
+  <h3>2011</h3>
+  
+  <ul>
+    <li>Timothy Hnat, Vijay Srinivasan, Jiakang Lu, Tamim Sookoor, Raymond Dawson, John Stankovic, and Kamin Whitehouse. <a href="docs/hnat11hitchhikers.pdf" onClick="javascript:pageTracker._trackPageview('docs/hnat11hitchhikers.pdf'); ">The Hitchhiker's Guide to Successful Residental Sensing Deployments</a>. The 9th ACM Conference on Embedded Networked Sensing Systems (SenSys'11). November 2-4, 2011, Seattle, WA. (<a href="docs/hnat-sensys11.pptx" onClick="javascript:pageTracker._trackPageview('docs/hnat-sensys11.pptx'); ">ppt</a>) [19.5% acceptance rate]
+    </li>
+  </ul>
+  
+  <h3>2010</h3>
+  
+  <ul>
+    <li>Jiakang Lu, Tamim Sookoor, Vijay Srinivasan, Gao Ge, Brian Holben, John Stankovic, Eric Field, and Kamin Whitehouse. <a href="docs/sensys10thermostat.pdf" onClick="javascript:pageTracker._trackPageview('docs/sensys10thermostat.pdf'); ">The Smart Thermostat: Using Occupancy Sensors to Save Energy in Homes</a>. The 8th ACM Conference on Embedded Networked Sensing Systems (SenSys'10). November 3-5, 2010, Zurich, Switzerland. (<a href="docs/sensys10lu_SmartThermostat-talk.ppt" onClick="javascript:pageTracker._trackPageview('docs/sensys10lu_SmartThermostat-talk.ppt'); ">ppt</a>) [17% acceptance rate]</li>
 
-<h3>2009</h3>
-
-<ul>
-
-<li>Tamim Sookoor, Timothy Hnat, Pieter Hooimeijer, Westley Weimer, and Kamin Whitehouse. <a href="docs/sookoor09macrodebugging.pdf" onClick="javascript:pageTracker._trackPageview('docs/sookoor09macrodebugging.pdf'); ">Macrodebugging: Global Views of Distributed Program Execution</a>. The 7th ACM Conference on Embedded Networked Sensor Systems (SenSys'09), November 2009, Berkeley, CA. (<a href="docs/mdb2.ppt" onClick="javascript:pageTracker._trackPageview('docs/mdb2.ppt'); ">ppt</a>) [17.6% acceptance rate] </li>
-
+    
+    <br />
+    
+    <li>Timothy W. Hnat, Tamim I. Sookoor, Pieter Hooimeijer, Westley Weimer, and Kamin Whitehouse. <a href="docs/hnat10compiler.pdf" onClick="javascript:pageTracker._trackPageview('docs/hnat10compiler.pdf'); ">A Modular and Extensible Macroprogramming Compiler</a>. The 1st Workshop on Software Engineering for Sensor Network Applications (SESENA) in Conjunction with ACM/IEEE International Conference on Software Engineering (ICSE), May 2010 Cape Town, South Africa. (<a href="docs/sookoor13iccpsFinal.pptx" onClick="javascript:pageTracker._trackPageview('docs/sookoor13iccpsFinal.pptx'); ">ppt</a>) </li>
+  </ul>
+  
+  <h3>2009</h3>
+  
+  <ul>
+    
+    <li>Tamim Sookoor, Timothy Hnat, Pieter Hooimeijer, Westley Weimer, and Kamin Whitehouse. <a href="docs/sookoor09macrodebugging.pdf" onClick="javascript:pageTracker._trackPageview('docs/sookoor09macrodebugging.pdf'); ">Macrodebugging: Global Views of Distributed Program Execution</a>. The 7th ACM Conference on Embedded Networked Sensor Systems (SenSys'09), November 2009, Berkeley, CA. (<a href="docs/mdb2.ppt" onClick="javascript:pageTracker._trackPageview('docs/mdb2.ppt'); ">ppt</a>) [17.6% acceptance rate] </li>
+    
 <br />
 
     <li>Timothy W. Hnat, Tamim I. Sookoor, and Kamin Whitehouse. <a href="docs/hnat09mdbdemo.pdf" onClick="javascript:pageTracker._trackPageview('docs/hnat09mdbdemo.pdf'); ">Demo Abstract: Macrodebugging with MDB</a>. The 7th ACM Conference on Embedded Networked Sensor Systems (SenSys'09), November, 2009, Berkeley, CA.</li>
 
-   <br />
-
-<li>Tamim Sookoor. <a href="docs/sookoor-masters-thesis.pdf" onClick="javascript:pageTracker._trackPageview('docs/sookoor-masters-thesis.pdf'); ">The Design of MDB: A Macrodebugger for Wireless Embedded Networks</a>. Master's Thesis, University of Virginia, 2009.</li>
-
-</ul>
-
-<h3>2008</h3>
-
+    <br />
+    
+    <li>Tamim Sookoor. <a href="docs/sookoor-masters-thesis.pdf" onClick="javascript:pageTracker._trackPageview('docs/sookoor-masters-thesis.pdf'); ">The Design of MDB: A Macrodebugger for Wireless Embedded Networks</a>. Master's Thesis, University of Virginia, 2009.</li>
+    
+  </ul>
+  
+  <h3>2008</h3>
+  
 <ul>
   <li>Timothy W. Hnat, Tamim I. Sookoor, Pieter Hooimeijer, Westley Weimer, and Kamin Whitehouse. <a href="docs/sensys08-macrolab.pdf" onClick="javascript:pageTracker._trackPageview('docs/sensys08-macrolab.pdf'); ">MacroLab: A Vector-based Macroprogramming Framework for Cyber-Physical Systems</a>. The 6th ACM Conference on Embedded Networked Sensor Systems (SenSys'08), November 2008, Raleigh, NC. (<a href="docs/sensys08macrolab.key" onClick="javascript:pageTracker._trackPageview('docs/sensys08macrolab.key'); ">key</a>, <a href="docs/sensys08macrolab.ppt" onClick="javascript:pageTracker._trackPageview('docs/sensys08macrolab.ppt'); ">ppt</a>, <a href="docs/mdb2.ppt" onClick="javascript:pageTracker._trackPageview('docs/mdb2.pdf'); ">pdf</a>) [16% acceptance rate]</li>
-
-<br />
-
-    <li>Tamim I. Sookoor, Timothy W. Hnat, and Kamin Whitehouse. <a href="docs/sensys08-macrolab-demo.pdf" onClick="javascript:pageTracker._trackPageview('docs/sensys08-macrolab-demo.pdf'); ">Demo Abstract: Programming Cyber-Physical Systems with MacroLab</a>. The 6th ACM Conference on Embedded Networked Sensor Systems (SenSys'08), November, 2008, Raleigh, NC.</li>
+  
+  <br />
+  
+  <li>Tamim I. Sookoor, Timothy W. Hnat, and Kamin Whitehouse. <a href="docs/sensys08-macrolab-demo.pdf" onClick="javascript:pageTracker._trackPageview('docs/sensys08-macrolab-demo.pdf'); ">Demo Abstract: Programming Cyber-Physical Systems with MacroLab</a>. The 6th ACM Conference on Embedded Networked Sensor Systems (SenSys'08), November, 2008, Raleigh, NC.</li>
 </ul>
 
 <h3>2007</h3>
 
 <ul>
-<li>L. Selavo, A. Wood, Q. Cao, T. Sookoor, H. Liu, A. Srinivasan, Y. Wu, W. Kang, J. Stankovic, D. Young, J. Porter. <a href="docs/sensys07-luster.pdf" onClick="javascript:pageTracker._trackPageview('docs/sensys07-luster.pdf'); ">LUSTER: Wireless Sensor Network for Environmental Research</a>. The 5th ACM Conference on Embedded Networked Sensor Systems (SenSys'07), November 2007, Sydney, Australia. [17% acceptance rate]</li>
+  <li>L. Selavo, A. Wood, Q. Cao, T. Sookoor, H. Liu, A. Srinivasan, Y. Wu, W. Kang, J. Stankovic, D. Young, J. Porter. <a href="docs/sensys07-luster.pdf" onClick="javascript:pageTracker._trackPageview('docs/sensys07-luster.pdf'); ">LUSTER: Wireless Sensor Network for Environmental Research</a>. The 5th ACM Conference on Embedded Networked Sensor Systems (SenSys'07), November 2007, Sydney, Australia. [17% acceptance rate]</li>
 </ul>
 
 <?php virtual("inc/footer.shtml") ?>
